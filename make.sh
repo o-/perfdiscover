5b661f99d4489cac430e386a583bf77b9e581229
o <o@o1o.ch>
2016-12-04 22:00:28 +0100

    fix make

--- make.sh
+++ make.sh
@@ -1,13 +1,13 @@
-gcc -std=c++11 -lstdc++ -I. main.cpp 
+g++ -std=c++11 -I. main.cpp 
   ./a.out -g 10 28 8 8 > gen.h 
   echo "WS,EL,T" > res.csv 
   echo 1
-  gcc -O1 -std=c++11 -lstdc++ -I. main.cpp 
+  g++ -O2 -std=c++11 -I. main.cpp 
   ./a.out >> res.csv 
   echo 2
-  gcc -O1 -std=c++11 -lstdc++ -I. main.cpp 
+  g++ -O2 -std=c++11 -I. main.cpp 
   ./a.out >> res.csv 
   echo 3
-  gcc -O2 -std=c++11 -lstdc++ -I. main.cpp 
-  ./a.out >> res.csv 
+  g++ -O2 -std=c++11 -I. main.cpp 
+   ./a.out >> res.csv 
   R -f plot.R
